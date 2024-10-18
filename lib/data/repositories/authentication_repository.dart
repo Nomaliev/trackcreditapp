0681666b2c6498cb541a91c30a79c2f253c10668
Tabriz <nomaliev04@gmail.com>
2024-10-18 20:00:06 +0400

    authentication repo && exceptions added

---
+++ lib/data/repositories/authentication_repository.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:trackcreditapp/utilities/constans/exceptions/exceptions.dart';
+import 'package:trackcreditapp/utilities/constans/exceptions/firebase_exceptions.dart';
+import 'package:trackcreditapp/utilities/constans/exceptions/platform_exceptions.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+  final _auth = FirebaseAuth.instance;
+
+  Future<UserCredential> registerWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      throw AppFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw AppFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const FormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:trackcreditapp/app.dart';
+import 'package:trackcreditapp/data/repositories/authentication_repository.dart';
 import 'package:trackcreditapp/firebase_options.dart';
 
 Future<void> main() async {
   runApp(const MyApp());
 
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
 }
